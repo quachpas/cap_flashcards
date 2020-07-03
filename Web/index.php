1932761c78695695ed1e9e7eb7c1711248fab010
Pascal <pascal.quach.utc@gmail.com>
2020-07-03 16:16:57 +0200

    web

--- Web/index.php
+++ Web/index.php
@@ -42,6 +42,9 @@ if (!empty($_FILES)) {
 	$actualName = $_FILES['file']['tmp_name'];
 	$actualSize = $_FILES['file']['size'];
 	$extension = strtolower(pathinfo($_FILES["file"]["name"], PATHINFO_EXTENSION));
+	echo(pathinfo($_FILES["file"]["name"])."</br>");
+	echo(strtolower(pathinfo($_FILES["file"]["name"], PATHINFO_EXTENSION))."</br>");
+	echo($legalExtensions."<br>");
 
 	$pathroot = '/tmp/upload/' . $id . '/';
 	$pathin = $pathroot . 'in/';
